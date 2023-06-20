ff475aa70f894c8ec93e5b9952613fbcd8fb9fcd
Michael Kuehn <michael.kuehn@uipath.com>
2023-06-20 16:39:50 -0400

    try to add net461

---
+++ UiPathTeam.Extensions.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace UiPathTeam.Extensions.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testActiuvity = new UiPathTeam.Extensions.Activities.Test
+            {
+
+            };
+
+
+        }
+    }
+}
