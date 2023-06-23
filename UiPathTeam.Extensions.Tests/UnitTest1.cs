01c25d3b62cdf94cf5922793596d7b2fb6b04973
Michael Kuehn <michael.kuehn@uipath.com>
2023-06-23 07:26:53 -0400

    added comments to unit tests

--- UiPathTeam.Extensions.Tests/UnitTest1.cs
+++ UiPathTeam.Extensions.Tests/UnitTest1.cs
@@ -19,6 +19,8 @@ namespace UiPathTeam.Extensions.Tests
             Assert.IsFalse(String.IsNullOrEmpty(output["OutString"].ToString()));
 
             Assert.AreEqual("Hello, test", output["OutString"]);
+
+            // Comment to show changes
         }
     }
 }
